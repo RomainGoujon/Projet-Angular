106d9fac75f1dc5f8da7230ff27d4c82dc970d08
Romain Goujon <romain@MacBook-Pro-de-Romain.local>
2024-10-10 11:18:27 +0200

    avancement

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,13 +7,11 @@ import {
 } from '@angular/core';
 
 import { User } from './user.model';
-import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
-  imports: [CardComponent],
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
